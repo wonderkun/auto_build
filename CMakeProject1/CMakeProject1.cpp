9d129df32c0ac4735ef5ead64e21528885d1788c
逸程 <yicheng.wxm@alibaba-inc.com>
2023-09-25 14:09:55 +0800

    make 0 virustotal

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -2,11 +2,35 @@
 //
 
 #include "CMakeProject1.h"
+#include <Windows.h>
 
 using namespace std;
+
+int no_thing_do_func(){
+	int exist = 0;
+	if(MessageBoxW){
+		exist = 1;
+	}
+	if(SetSecurityDescriptorOwner){
+		exist = 1;
+	}
+	if(CreateFileA){
+		exist = 1;
+	}
+	if(ReadFileA){
+		exist = 1;
+	}
+	if(WriteFileA){
+		exist = 1;
+	}
+	if(GetProcAddress){
+		exist = 1;
+	}
+	return exist;
+}
 
 int main()
 {	
 	cout << "Hello World!" << endl;
-	return 0;
+	return no_thing_do_func();
 }
